a605d4451fd351ef27271d437104dfb2dde94076
Ajeigbe Abdulhakeem <ajeigbehakeem@yahoo.com>
2024-12-14 20:16:45 +0100

    Updated website with new changes

--- js/script.js
+++ js/script.js
@@ -19,13 +19,13 @@ document.getElementById('hamburger-icon').addEventListener('click', function () 
 emailjs.init("GE8eROodzjpRsN9wH");
 
 document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form default action
+    event.preventDefault(); // Prevent default form submission
 
     emailjs.sendForm("service_gc94r0o", "template_d5vu3vc", this)
         .then(function (response) {
             alert("Message sent successfully!");
             console.log("SUCCESS!", response.status, response.text);
-            document.getElementById("contact-form").reset(); // Reset the form after success
+            document.getElementById("contact-form").reset(); // Reset form after success
         }, function (error) {
             alert("Failed to send message. Please try again.");
             console.error("FAILED...", error);
